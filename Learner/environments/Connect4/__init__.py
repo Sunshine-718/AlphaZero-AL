8a0646fde65c2b47def90e93077a95b1a4d9deee
sunshine-718 <twq566256@foxmail.com>
2025-08-11 20:34:24 +1000

    clean learner

--- Learner/environments/Connect4/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import training_config, network_config, env_config
-from .env_cython import Env
-from .Network import CNN, ViT
-from .utils import instant_augment, inspect
--- Learner/environments/__init__.py
+++
@@ -1,5 +0,0 @@
-import importlib
-
-def load(env_name):
-    return importlib.import_module(f"environments.{env_name}")
-    
