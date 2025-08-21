a466b256e817f08ec2b4097841f7586c72fe263a
sunshine-718 <twq566256@foxmail.com>
2025-08-22 01:36:53 +1000

    clean comments

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ import shutil
 import os
 
 try:
-    # 支持多个 cython 模块编译
     ext_modules = cythonize(
-        ["./src/env_cython.pyx"], 
+        ["./src/env_cython.pyx"],
         compiler_directives={
             "language_level": "3",
             "boundscheck": False,
@@ -22,7 +21,6 @@ try:
 except Exception as e:
     print(f"[Error] Cython 编译失败: {e}")
 finally:
-    # 编译后移动 .pyd/.so 文件
     folder = './src/'
     for filename in os.listdir(folder):
         if '.cpp' in filename:
